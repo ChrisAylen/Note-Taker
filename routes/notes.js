ac9b0e22420bed3f3c5dfb883fbd8bf6b1b5bf61
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-05-28 09:34:55 +0100

    Get by ID and Delete by ID routed added

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ const {
     readFromFile,
     readAndAppend,
     writeToFile,
-  } = require('../helpers/fsUtils');
+} = require('../helpers/fsUtils');
 
 
 // api/notes routes
@@ -16,56 +16,65 @@ notes.get('/', (req, res) =>
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data))).then(() => console.log('GET /api/notes'))
 );
 
+//GET route to return record by Id
+notes.get('/:noteId', (req, res) => {
+    const noteId = req.params.noteId;
+    readFromFile('./db/db.json').then((data) => JSON.parse(data)).then((json) => {
+        const result = json.filter((note) => note.note_id === noteId);
+        res.json(result);
+    })
+});
+
 
 //POST route receives a new note to save on the request body, add it to the db.json file, and then return the new note to the client
 
 notes.post('/', (req, res) => {
     // Destructuring assignment for the items in req.body
     // for (let info of Object.entries(req.body)) {
-        const { text, title } = req.body;
-        //let test = req.body[0].text;
+    const { text, title } = req.body;
+    //let test = req.body[0].text;
 
 
-        // If all the required properties are present
-        if (title && text) {
-            // Variable for the object we will save
-            const newNote = {
-                title,
-                text,
-                note_id: uuidv4(),
-            };
+    // If all the required properties are present
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
 
-            readAndAppend(newNote, './db/db.json');
+        readAndAppend(newNote, './db/db.json');
 
-            const response = {
-                status: 'success',
-                body: newNote,
-            };
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
 
-            res.json(response);
-        } else {
-            res.json('Error in posting note');
-        }
+        res.json(response);
+    } else {
+        res.json('Error in posting note');
+    }
     //}
 });
 //Each note should have a unique id
 
 //Delete /api/notes/:id
-notes.delete('/:Id', (req, res) => {
-    const Id = req.params.id;
+notes.delete('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
     readFromFile('./db/db.json')
-      .then((data) => JSON.parse(data))
-      .then((json) => {
-        // Make a new array of all notes except the one with the ID provided in the URL
-        const result = json.filter((note) => note.note_id !== Id);
-  
-        // Save that array to the filesystem
-        writeToFile('./db/notes.json', result);
-  
-        // Respond to the DELETE request
-        res.json(`Item ${Id} has been deleted 🗑️`);
-      });
-  });
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            // Make a new array of all notes except the one with the ID provided in the URL
+            const result = json.filter((note) => note.note_id !== noteId);
+
+            // Save that array to the filesystem
+            writeToFile('./db/db.json', result);
+
+            // Respond to the DELETE request
+            res.json(`Item ${noteId} has been deleted 🗑️`);
+        });
+});
 
 
 module.exports = notes;
